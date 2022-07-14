90d483792c105584ea131ba1f4fca8051f96c5dc
Lucas Dias <lucaalencarID@gmail.com>
2022-07-13 22:42:18 -0300

    Todos os bonus concluido

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const email = document.getElementById('email');
+const emails = document.getElementById('email');
 const senha = document.getElementById('senha');
 const login = document.getElementById('login');
 
 function verificaAcesso() {
-  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+  if (emails.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
@@ -25,3 +25,58 @@ function check() {
 }
 
 checkBox.addEventListener('click', check);
+
+const areaTexto = document.querySelector('#textarea');
+const counter = document.querySelector('#counter');
+
+function contador() {
+  const tam = areaTexto.value.length;
+  const atl = 500 - tam;
+  counter.innerHTML = atl;
+}
+areaTexto.addEventListener('input', contador);
+function getData() {
+  const materias = [];
+  for (let index = 7; index <= 12; index += 1) {
+    const tmp = document.forms[1][index];
+    if (tmp.checked === true) {
+      materias.push(tmp.value);
+    }
+  }
+  console.log(materias);
+  const nome = document.querySelector('#input-name').value;
+  const sobrenome = document.querySelector('#input-lastname').value;
+  const email = document.querySelector('#input-email').value;
+  const casa = document.querySelector('#house').value;
+  const familia = document.querySelector('input[name="family"]:checked').value;
+  const avaliaçao = document.querySelector('input[name="rate"]:checked').value;
+  const observaçao = document.querySelector('#textarea').value;
+  return [nome, sobrenome, email, casa, familia, materias, avaliaçao, observaçao];
+}
+
+function criaForm(dados) {
+  const main = document.querySelector('main');
+  const infoPara = document.createElement('ul');
+  main.appendChild(infoPara);
+  infoPara.id = 'form-data';
+  const data = [`Nome: ${dados[0]} ${dados[1]}`,
+    `Email: ${dados[2]}`, `Casa: ${dados[3]}`, `Família: ${dados[4]}`,
+    `Matérias: ${dados[5][0]}, ${dados[5][1]}, ${dados[5][2]}`, `Avaliação: ${dados[6]}`,
+    `Observações: ${dados[7]}`];
+  for (let index = 0; index < 7; index += 1) {
+    const tmp = document.createElement('li');
+    tmp.innerText = data[index];
+    infoPara.appendChild(tmp);
+  }
+}
+
+function botton(event) {
+  event.preventDefault();
+  const dataForm = getData();
+  document.querySelector('#evaluation-form').style.display = 'none';
+  criaForm(dataForm);
+}
+btnEnviar.addEventListener('click', botton);
+
+// getData();
+// form.style.display = 'none';
